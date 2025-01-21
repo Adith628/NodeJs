5587d5769c7959e2b681ab1a2d61239cfd921885
Adithyan T <adithyant.mail@gmail.com>
2025-01-22 01:01:39 +0530

    updated

---
+++ index.js
@@ -0,0 +1,5 @@
+const { generateRandomNumber, celciusToFahrenheit } = require("./utlis");
+
+console.log("Random number : ", generateRandomNumber());
+
+console.log("Celcius to Fahrenheit : ", celciusToFahrenheit(0));
