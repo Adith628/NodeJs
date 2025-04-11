79f2dd3db19fa2416d6d9e0a6690d82ba521a16c
Adith628 <adithsreedhar628@gmail.com>
2025-04-12 01:52:41 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { generateRandomNumber, celciusToFahrenheit } = require("./utlis");
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utlis");
 
-console.log("Random number : ", generateRandomNumber());
+// console.log("Random number : ", generateRandomNumber());
 
-console.log("Celcius to Fahrenheit : ", celciusToFahrenheit(0));
+// console.log("Celcius to Fahrenheit : ", celciusToFahrenheit(0));
+
+import { getPosts } from "./postController.js";
+
+console.log(getPosts());
---
+++ server.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World<h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
